28b8dc25ef95743e5e3c61c384c779eae4c5705f
Kirill Shcherbakov <50148272+CyrilShch@users.noreply.github.com>
2021-07-08 14:36:38 +0200

    Update script_MediaMarkt.py

--- Personalization/script_MediaMarkt.py
+++ Personalization/script_MediaMarkt.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import FirefoxOptions
 import pandas as pd
 import numpy as np
 import time
@@ -105,8 +106,10 @@ def iteration_mobile(driver, item, delays, collected_data):
 
                 # temporary dictionary of the product data
                 temp = {
+                    'website': "MediaMarkt",
                     'item': item,
                     'product': product_name[0].text,
+                    'time': pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S"),
                     'price': price
                 }
 
@@ -160,8 +163,10 @@ def iteration(driver, item, delays, collected_data):
 
                 # temporary dictionary of the product data
                 temp = {
+                        'website': "MediaMarkt",
                         'item': item,
                         'product': product_name[0].text,
+                        'time': pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S"),
                         'price': price
                         }
 
@@ -200,8 +205,11 @@ def main(params):
             "sslProxy": PROXY,
             "proxyType": "MANUAL",
         }
+        
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
     # initialize a webdriver
-    driver = webdriver.Firefox(profile, executable_path=params.exec_path)
+    driver = webdriver.Firefox(firefox_options=opts, firefox_profile=profile)
     # get the url
     driver.get(params.web_page)
 
@@ -286,7 +294,7 @@ def main(params):
 
     print("Writing csv file...")
     df = pd.DataFrame(collected_data)
-    df.to_csv(f'{params.exp_name}.csv', index=False)
+    df.to_csv(f'{params.exp_name}' + '_' + pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S") + ".csv", index=False)
     print("Writing finished.")
 
     # close the driver
@@ -299,4 +307,3 @@ if __name__ == '__main__':
 
     # run the script
     main(params)
-
